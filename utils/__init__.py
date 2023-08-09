97f17ce164afd59b43f1225079ffc0aa1b23f5a8
Julian Pleines <julian.pleines@gmail.com>
2023-08-09 10:35:00 +0200

    closes #1

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,20 +1,40 @@
 # missuse of the file to check if external imports are available and installing them on demand
+import os
 import subprocess
 import sys
+from typing import List
 
 import adsk.core
 
-app = adsk.core.Application.get()
+from ..lib import fusion360utils as futil
 
+app = adsk.core.Application.get()
 try:
     import numpy
 
-    # print("Numpy found, uninstalling it now")
-    # reqs = subprocess.run([sys.executable, '-m', 'pip', 'uninstall', 'numpy'],stdout=subprocess.PIPE,stderr=subprocess.PIPE, shell=True, timeout=40)
-    # print("Done")
+    futil.log("Numpy found", adsk.core.LogLevels.InfoLogLevel, True)
 except ModuleNotFoundError:
-    print("Numpy not found, installing it now")
-    # reqs = subprocess.run([sys.executable, '-m', 'pip', 'install', 'numpy'], check=True, capture_output=True, shell=False, timeout=20)
-    reqs = subprocess.call([sys.executable, '-m', 'pip', 'install', 'numpy'], shell=True, timeout=20)
-    print("Done")
-    import numpy
+    futil.log("Numpy not found, installing it now, this may take a while...", adsk.core.LogLevels.InfoLogLevel, True)
+    if (sys.executable.find("Fusion360") != -1 and sys.executable.endswith("exe")):
+        executable: str = sys.executable[:sys.executable.find("Fusion360")]
+        executable += os.sep + "Python" + os.sep + "python.exe"
+
+        reqs = subprocess.run([executable, '-m', 'pip', 'install', 'numpy'], check=True, capture_output=True, shell=False, timeout=40)
+        import numpy
+        futil.log("Done", adsk.core.LogLevels.InfoLogLevel, True)
+    elif (sys.executable.find("python") != -1):
+
+        reqs = subprocess.run([sys.executable, '-m', 'pip', 'install', 'numpy'], check=True, capture_output=True, shell=False, timeout=40)
+        import numpy
+        futil.log("Done", adsk.core.LogLevels.InfoLogLevel, True)
+    else:
+        directoryList: List[str] = sys.executable.split(os.sep)[:-1]
+        directory: str = ""
+        for element in directoryList:
+            directory = directory + element + os.sep
+        futil.log(">numpy< could not be installed automatically, this may happen if you are working on Mac. Try to manually install the python module for Fusion360's python version.\n" + 
+                  "To do so, navigate to this directory: " + directory + "\n" + 
+                  "Navigate to the >Python< directory in there \n" +
+                  "Within a terminal, execute the Python application with the following parameters: \n" +
+                  "-m pip install numpy\n" + "Rerun this Plugin.", adsk.core.LogLevels.ErrorLogLevel, True)
+
